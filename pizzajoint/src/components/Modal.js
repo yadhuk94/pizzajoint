45a96cc9dbcf0c160e4c684ddb12fec041189c8b
Yadhu Krishnan <yadhuk94@gmail.com>
2020-06-13 18:02:58 +0530

    pizza joint final

---
+++ pizzajoint/src/components/Modal.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
+const backdrop = {
+  visible: {
+    opacity: 1,
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+
+const modal = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: 200,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+    },
+  },
+};
+
+const Modal = ({ showModal, setShowModal }) => {
+  return (
+    <AnimatePresence exitBeforeEnter>
+      {showModal && (
+        <motion.div
+          className="backdrop"
+          variants={backdrop}
+          animate="visible"
+          initial="hidden"
+          exit="hidden"
+        >
+          <motion.div className="modal" variants={modal}>
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button>Start Again</button>
+            </Link>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Modal;
